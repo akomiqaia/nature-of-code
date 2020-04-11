33ad687da5850d47e3ab2dfae663811fb0ccc307
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-04-11 13:54:15 +0100

    add vertex shape to display waveforms with noise function in 1D perlin noise

--- 0 - Introduction/1- perlin .js
+++ 0 - Introduction/1- perlin .js
@@ -3,14 +3,22 @@ let xoff2 = 10000
 
 function setup() {
   createCanvas(400, 400);
-  background(51);
 }
-
+let inc = 0.01;
+let start = 0;
 function draw() {
-  let x = map(noise(xoff1), 0, 1, 0, width )
-  let y = map(noise(xoff2), 0, 1, 0, height )
-  
-  xoff1 += 0.01
-  xoff2 += 0.01
-  ellipse(x, y, 24, 24);
+  background(51);
+  stroke(255);
+  noFill();
+  beginShape();
+  let xoff = start;
+  for (let x = 0; x < width; x++) {
+    stroke(255);    
+    let y = noise(xoff) * height;
+    vertex(x, y)
+
+    xoff += 0.02
+  }
+  endShape()
+  start += 0.02
 }
