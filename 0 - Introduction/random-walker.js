6083a03daee2c3955bbd914c9ffbb6e3598b13e1
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-04-12 22:15:17 +0100

    add second point and let them mirror each other

--- 0 - Introduction/random-walker.js
+++ 0 - Introduction/random-walker.js
@@ -6,28 +6,35 @@ function setup() {
   createCanvas(800, 800)
   x = width / 2;
   y = height / 2;
+  z = width / 2;
+  w = height / 2;
   background(51);
 }
 
 function draw() {
     point(x, y);
+    point(z, w);
     let randomDirection = floor(random(4)) 
-    
+    let r = float(random(1))
     switch (randomDirection){
           case 0:
-            x = x + 10
+            x = x + 5
+            z = z - 5
             drawColoredRandomWalkerNoise()
           break;
           case 1:
-            x = x - 10
+            x = x - 5
+            z = z + 5
             drawColoredRandomWalkerNoise()
           break;
           case 2:
-            y = y + 10
+            y = y + 5
+            w = w + 5
             drawColoredRandomWalkerNoise()
           break;
           case 3:
-            y = y - 10
+            y = y - 5
+            w = w - 5
             drawColoredRandomWalkerNoise()
           break;
         }
@@ -55,5 +62,5 @@ function drawColoredRandomWalkerNoise() {
   let g = noise(inc + 20)*255
   let b = noise(inc + 40)*255
   stroke(r,g,b);
-  strokeWeight(10);
+  strokeWeight(5);
 }
